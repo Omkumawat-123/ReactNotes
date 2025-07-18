e4ab87db3c87680b3e8ab9676ff65ba2495201a0
Om kumawat <126804270+Omkumawat-123@users.noreply.github.com>
2025-07-18 11:31:44 +0530

    lezy loading using SetTimeout()

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React ,  {useState}from 'react'
+import React, { useState } from "react";
 
 function App() {
-   
-  let [state, SetState]=useState(1);  // State declare  initally its 1 
-   console.log(state);    // console log 
+  let [state, SetState] = useState(1); // State declare  initally its 1
+  console.log(state); // console log
   return (
     <div>
-      <h1 onClick={()=> SetState(state*10) }> {state}  </h1> 
-    </div>//we can't directly change the state like this: state = 10 
+      <h1 onClick= {() =>  setTimeout(() => SetState(state + 1), 1000)}> State : {state} </h1>
+    </div>
+    //we can't directly change the state like this: state = 10
     // Suppose we want to change the state — we should do it using SetState(10) instead of directly writing state = 10, because React doesn't allow direct state updates
-    
-  )
-  
+  );
 }
 
-export default App
+export default App;
